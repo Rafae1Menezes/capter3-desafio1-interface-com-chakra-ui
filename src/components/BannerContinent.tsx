32a0cc253e76fb0ab0080a87219f511c3e455aac
Rafael Menezes <menezes.inbox@gmail.com>
2022-01-22 20:38:37 -0300

    responsividade do banner

--- src/components/BannerContinent.tsx
+++ src/components/BannerContinent.tsx
@@ -5,19 +5,19 @@ interface BannerContinentProps {
 }
 
 export default function BannerContinent({ titulo }: BannerContinentProps) {
-    const [isLargerThan715] = useMediaQuery('(min-width: 715px)')
+    const [isLargerThan990] = useMediaQuery('(min-width: 990px)')
 
     return (
         <Box
             backgroundImage="url('/europabanner.jpg')"
             backgroundSize="cover"
             backgroundPosition="bottom"
-            height="500px"
+            height={{base: "150px", lg:"500px"}}
         >
             <Container maxWidth="1160px" height="100%" position="relative">
 
                 {
-                    isLargerThan715
+                    isLargerThan990
                         ? <Heading
                             color="gray.100"
                             fontSize="48px"
